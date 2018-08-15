86294f6267edaa707acd650dda10c144e901bc65
Hang <yhnbgfd@gmail.com>
2018-08-15 15:55:39 +0800

    =

--- Hang.BaiduAI.FaceWeb/FaceApi.cs
+++ Hang.BaiduAI.FaceWeb/FaceApi.cs
@@ -9,7 +9,7 @@ namespace Hang.BaiduAI.FaceWeb
 {
     public class FaceApi
     {
-        Face face;
+        private Face face;
 
         public void Init(string apiKey, string secretKey)
         {
@@ -44,9 +44,9 @@ namespace Hang.BaiduAI.FaceWeb
                 {
                     {"image", cameraImageBase64},
                     {"image_type", "BASE64"},
-                    {"face_type", "LIVE"},
-                    {"quality_control", "NORMAL"},
-                    {"liveness_control", "NORMAL"},
+                    {"face_type", "LIVE"},//人脸的类型 LIVE表示生活照：通常为手机、相机拍摄的人像图片、或从网络获取的人像图片等，IDCARD表示身份证芯片照：二代身份证内置芯片中的人像照片， WATERMARK表示带水印证件照：一般为带水印的小图，如公安网小图 CERT表示证件照片：如拍摄的身份证、工卡、护照、学生证等证件图片 默认LIVE
+                    {"quality_control", "NORMAL"},//图片质量控制 NONE: 不进行控制 LOW:较低的质量要求 NORMAL: 一般的质量要求 HIGH: 较高的质量要求 默认 NONE
+                    {"liveness_control", "NORMAL"},//活体检测控制 NONE: 不进行控制 LOW:较低的活体要求(高通过率 低攻击拒绝率) NORMAL: 一般的活体要求(平衡的攻击拒绝率, 通过率) HIGH: 较高的活体要求(高攻击拒绝率 低通过率) 默认NONE
                 },
                 new JObject
                 {
